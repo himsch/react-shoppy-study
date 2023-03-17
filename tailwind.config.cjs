0a87bfaab1ab12698a88125918ac71c655d24b1e
song <55rsearchco@gmail.com>
2023-03-17 13:58:33 +0900

    라우터(헤더) 만들기💡- 스타일링

---
+++ tailwind.config.cjs
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        brand: '#F96162'
+      }
+    },
+  },
+  plugins: [],
+}
