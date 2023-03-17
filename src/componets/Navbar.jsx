f3b7c086a6ef3f7cb05836ea5d4326eeb31d326b
song <55rsearchco@gmail.com>
2023-03-17 15:35:36 +0900

    로그인,로그아웃 로직 리팩토링

--- src/componets/Navbar.jsx
+++ src/componets/Navbar.jsx
@@ -8,18 +8,9 @@ function Navbar(props) {
   const [user, setUser] = useState(null);
 
   useEffect(() => {
-    onUserStateChange(user => {
-      console.log(user);
-      setUser(user);
-    });
+    onUserStateChange(setUser);
   }, []);
 
-  const handleLogin = () => {
-    login().then(setUser);
-  };
-  const handleLogout = () => {
-    logout().then(setUser);
-  };
   return (
     <header className="flex justify-between border-b border-gray-300 p-2">
       <Link to="/" className="flex items-center text-4xl text-brand">
@@ -32,8 +23,8 @@ function Navbar(props) {
         <Link to="/products/new" className="text-2xl">
           <BsFillPenFill />
         </Link>
-        {!user && <button onClick={handleLogin}>Login</button>}
-        {user && <button onClick={handleLogout}>Logout</button>}
+        {!user && <button onClick={login}>Login</button>}
+        {user && <button onClick={logout}>Logout</button>}
       </nav>
     </header>
   );
