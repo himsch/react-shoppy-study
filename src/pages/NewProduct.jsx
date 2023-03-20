fa698dca3e079c322a7052026fb4ee69a5f9f93d
song <55rsearchco@gmail.com>
2023-03-20 14:03:08 +0900

    새로운 제품 등록 - 입력폼

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -1,7 +1,80 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../componets/ui/Button.jsx';
 
 function NewProduct(props) {
-  return <div>NewProduct</div>;
+  const [product, setProduct] = useState({});
+  const [file, setFile] = useState();
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    // 제품의 사진을 Cloudinary에 업로드 하고 URL를 획득한다.
+    // Firebase에 새로운 제품을 추가한다.
+  };
+  const handleChange = e => {
+    const { name, value, files } = e.target;
+    if (name === 'file') {
+      setFile(files && files[0]);
+      return;
+    }
+    setProduct(product => ({ ...product, [name]: value }));
+  };
+
+  return (
+    <section>
+      {file && <img src={URL.createObjectURL(file)} alt="local file" />}
+      <form onSubmit={handleSubmit}>
+        <h1>새로운 제품 입력</h1>
+        <input
+          type="file"
+          accept="image/*"
+          name="file"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="title"
+          value={product.title ?? ''}
+          placeholder="제품명"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="number"
+          name="price"
+          value={product.price ?? ''}
+          placeholder="가격"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="category"
+          value={product.category ?? ''}
+          placeholder="카테코리"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="description"
+          value={product.description ?? ''}
+          placeholder="제품 설명"
+          required
+          onChange={handleChange}
+        />
+        <input
+          type="text"
+          name="options"
+          value={product.options ?? ''}
+          placeholder="옵션들(콤마(,)로 구분)"
+          required
+          onChange={handleChange}
+        />
+        <Button text={'제품 등록하기'} />
+      </form>
+    </section>
+  );
 }
 
 export default NewProduct;
