81cc7ab4b58295e1401a7220677c9783de290a23
song <55rsearchco@gmail.com>
2023-03-22 13:16:26 +0900

    제품 상세 페이지

--- src/componets/ProductCard.jsx
+++ src/componets/ProductCard.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-function ProductCard({ product: { id, image, title, category, price } }) {
+function ProductCard({ product }) {
+  const { id, image, title, category, price } = product;
+  const navigate = useNavigate();
+  const goDetail = () => {
+    navigate(`/products/${id}`, { state: { product } });
+  };
   return (
-    <li className="rounded-lg shadow-md overflow-hidden cursor-pointer">
+    <li
+      className="rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105"
+      onClick={goDetail}
+    >
       <img className="w-full" src={image} alt={title} />
       <div className="mt-2 px-2 text-lg flex justify-between items-center">
         <h3 className="truncate">{title}</h3>
